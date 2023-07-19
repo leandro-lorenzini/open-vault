579c18a428729a13743caff7cefc54527a5e42df
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-07-19 16:15:16 +0800

    Fixed loading problems with Windows client

--- gui/src/index.js
+++ gui/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const originalLog = console.log;
 console.log = (message) => {
@@ -27,8 +27,8 @@ const safeStringify = (arg) => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
