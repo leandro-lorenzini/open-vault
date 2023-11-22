b57db207536c9e1dd3eeeefec732b1b2e09c6051
Leandro Lorenzini <leandro.lorenzini@outlook.com>
2023-11-22 16:36:52 +0800

    Fixed issue with application icon and changed Product name to OpenVault

--- gui/src/components/LoadingView.js
+++ gui/src/components/LoadingView.js
@@ -1,9 +1,11 @@
 import { Typography } from "antd";
+import icon from "./icon.png";
+import iconWhite from "./icon-white.png";
 
 export default function LoadingView(props) {
 	return (
 		<div style={{ textAlign: 'center' }}>
-			<img src={props.darkMode ? "/icons/icon-white.png":"/icons/icon.png"} width={70}/>
+			<img src={props.darkMode ? iconWhite:icon} width={70}/>
 			<Typography.Title>Open-Vault</Typography.Title>
 			<Typography.Text level={4}>Loading...</Typography.Text>
 		</div>
